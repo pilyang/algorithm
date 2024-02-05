74abf5e4f16d0109ecc962f204159119fd1d05d6
pilyang <pilyang.dev@gmail.com>
2024-02-05 20:51:39 +0900

    solve: leet 387 - seccond

--- golang/leetCode/387_firstUniqueCharactorInString/solution.go
+++ golang/leetCode/387_firstUniqueCharactorInString/solution.go
@@ -4,24 +4,16 @@ package uniqueCharacter
 
 func firstUniqChar(s string) int {
 	runeCount := make(map[rune]int)
-	runeIndex := make(map[rune]int)
+
+	for _, r := range s {
+		runeCount[r]++
+	}
 
 	for i, r := range s {
-		runeCount[r]++
-		if _, exists := runeIndex[r]; !exists {
-			runeIndex[r] = i
+		if runeCount[r] == 1 {
+			return i
 		}
 	}
 
-	mi := len(s)
-	for r, c := range runeCount {
-		if c == 1 && runeIndex[r] < mi {
-			mi = runeIndex[r]
-		}
-	}
-
-	if mi == len(s) {
-		return -1
-	}
-	return mi
+	return -1
 }
