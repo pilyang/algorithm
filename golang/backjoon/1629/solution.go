18cfb4ada805eb23a6c9b0e1456e3cb7551ad633
pilyang <pilyang.dev@gmail.com>
2024-02-18 19:59:14 +0900

    solve: bj 1629

---
+++ golang/backjoon/1629/solution.go
@@ -0,0 +1,50 @@
+// https://www.acmicpc.net/problem/1629
+// math
+
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var (
+	scanner *bufio.Scanner
+	writer  *bufio.Writer
+)
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+	writer = bufio.NewWriter(os.Stdout)
+	scanner.Split(bufio.ScanWords)
+}
+
+func nextInt() int {
+	scanner.Scan()
+	val, _ := strconv.Atoi(scanner.Text())
+	return val
+}
+
+// a ^ n % modNum
+// (a x b) mod m = (a mod m x b mod m) mod m
+// (a^n) mod m = (a^(n/2) x a^(n/2)) mod m
+func main() {
+	defer writer.Flush()
+
+	a, n, modNum := nextInt(), nextInt(), nextInt()
+
+	result := 1
+	for n > 0 {
+		if n%2 == 1 {
+			result = (result * a) % modNum
+		}
+		// calculate a^2
+		a = (a * a) % modNum
+		n /= 2
+	}
+	result %= modNum
+
+	writer.WriteString(strconv.Itoa(result))
+	writer.WriteByte('\n')
+}
