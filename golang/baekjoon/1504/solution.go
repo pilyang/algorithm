401bacc5401053327ce2e2f399f166c734a180d0
pilyang <pilyang.dev@gmail.com>
2024-04-09 01:25:30 +0900

    resolve: bj 1504
    - gold 4
    - floyd warshall -> dijkstra

--- golang/baekjoon/1504/solution.go
+++ golang/baekjoon/1504/solution.go
@@ -1,10 +1,11 @@
 // https://www.acmicpc.net/problem/1504
-// graph, floyd warshall
+// graph, floyd warshall -> dijkstra
 
 package main
 
 import (
 	"bufio"
+	"container/heap"
 	"math"
 	"os"
 	"strconv"
@@ -27,59 +28,92 @@ func scanInt() int {
 	return n
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
+type Edge struct {
+	to, cost int
+}
+
+type PriorityQueue []Edge
+
+func (p *PriorityQueue) Len() int               { return len(*p) }
+func (p *PriorityQueue) Less(i int, j int) bool { return (*p)[i].cost < (*p)[j].cost }
+func (p *PriorityQueue) Swap(i int, j int)      { (*p)[i], (*p)[j] = (*p)[j], (*p)[i] }
+func (p *PriorityQueue) Push(x any)             { *p = append(*p, x.(Edge)) }
+func (p *PriorityQueue) Pop() any {
+	e := (*p)[len(*p)-1]
+	*p = (*p)[:len(*p)-1]
+	return e
+}
+
+var (
+	pq       PriorityQueue
+	graph    [][]Edge
+	distance []int
+)
+
+func dijkstra(from, to int) int {
+	for i := range distance {
+		distance[i] = math.MaxInt
 	}
-	return b
+	pq = pq[:0]
+
+	distance[from] = 0
+	heap.Push(&pq, Edge{to: from, cost: 0})
+	for pq.Len() > 0 {
+		current := heap.Pop(&pq).(Edge)
+		if distance[current.to] < current.cost {
+			continue
+		}
+
+		for _, next := range graph[current.to] {
+			newDist := distance[current.to] + next.cost
+			if newDist < distance[next.to] {
+				distance[next.to] = newDist
+				heap.Push(&pq, Edge{to: next.to, cost: newDist})
+			}
+		}
+	}
+
+	return distance[to]
 }
 
 func main() {
 	defer writer.Flush()
 
 	n, e := scanInt(), scanInt()
-	graph := make([][]int, n+1)
+	graph = make([][]Edge, n+1)
 	for i := 1; i <= n; i++ {
-		graph[i] = make([]int, n+1)
-		for j := 1; j <= n; j++ {
-			if i == j {
-				continue
-			}
-			graph[i][j] = math.MaxInt
-		}
+		graph[i] = make([]Edge, 0)
 	}
+	distance = make([]int, n+1)
+	pq = make(PriorityQueue, 0, n)
 
 	for i := 0; i < e; i++ {
 		from, to, cost := scanInt(), scanInt(), scanInt()
-		graph[from][to] = int(cost)
-		graph[to][from] = int(cost)
+		graph[from] = append(graph[from], Edge{to: to, cost: cost})
+		graph[to] = append(graph[to], Edge{to: from, cost: cost})
 	}
 
 	wp1, wp2 := scanInt(), scanInt()
 
-	for k := 1; k <= n; k++ {
-		for from := 1; from <= n; from++ {
-			if from == k {
-				continue
-			}
-			for to := 1; to <= n; to++ {
-				if to == k || to == from {
-					continue
-				}
-				if graph[from][k] == math.MaxInt || graph[k][to] == math.MaxInt {
-					continue
-				}
-				graph[from][to] = min(graph[from][to], graph[from][k]+graph[k][to])
-			}
-		}
-	}
-
 	result := math.MaxInt
-	for _, path := range [][2]int{{wp1, wp2}, {wp2, wp1}} {
-		if graph[1][path[0]] == math.MaxInt || graph[path[0]][path[1]] == math.MaxInt || graph[path[1]][n] == math.MaxInt {
+	for _, testCase := range [][][]int{
+		{{1, wp1}, {wp1, wp2}, {wp2, n}},
+		{{1, wp2}, {wp2, wp1}, {wp1, n}},
+	} {
+		flag := false
+		dist := 0
+		for _, path := range testCase {
+			temp := dijkstra(path[0], path[1])
+			if temp == math.MaxInt {
+				flag = true
+				break
+			}
+			dist += temp
+		}
+		if flag {
 			continue
 		}
-		result = min(result, graph[1][path[0]]+graph[path[0]][path[1]]+graph[path[1]][n])
+		result = minInt(result, dist)
 	}
 	if result == math.MaxInt {
 		result = -1
@@ -87,4 +121,11 @@ func main() {
 
 	writer.WriteString(strconv.Itoa(result))
 	writer.WriteByte('\n')
+}
+
+func minInt(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
