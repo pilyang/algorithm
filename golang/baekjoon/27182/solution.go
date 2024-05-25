fd3dbf117fe793d6a8b5c8b842f6ad076ceed9c7
pilyang <pilyang.dev@gmail.com>
2024-05-25 21:42:06 +0900

    solve: bj 27182
    - bronze 4
    - math

---
+++ golang/baekjoon/27182/solution.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var (
+	scanner *bufio.Scanner
+	writer  *bufio.Writer
+)
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	writer = bufio.NewWriter(os.Stdout)
+}
+
+func scanInt() int {
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	return n
+}
+
+func main() {
+	defer writer.Flush()
+
+	n := scanInt()
+	m := scanInt()
+
+	result := n - 7
+	if result <= 0 {
+		result = m + 7
+	}
+
+	writer.WriteString(strconv.Itoa(result))
+	writer.WriteByte('\n')
+}
