8d8cd501c9157ea764fb8e7b7120bf614081b907
pilyang <pilyang.dev@gmail.com>
2024-06-04 20:46:45 +0900

    solve: bj 8393
    - bronze 5
    - math

---
+++ golang/baekjoon/8393/solution.go
@@ -0,0 +1,35 @@
+// sum, math
+
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var (
+	scanner *bufio.Scanner
+	writer  *bufio.Writer
+)
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+	writer = bufio.NewWriter(os.Stdout)
+}
+
+func scanInt() int {
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	return n
+}
+
+func main() {
+	defer writer.Flush()
+
+	n := scanInt()
+	sum := n * (n + 1) / 2
+
+	writer.WriteString(strconv.Itoa(sum))
+	writer.WriteByte('\n')
+}
